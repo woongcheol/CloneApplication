5cc60d5a0268ad903117fc0c200ab96895d93caa
Woongcheol Shin <1994_100@naver.com>
2022-04-10 01:14:22 +0900

    Initial commit

---
+++ app/src/main/java/com/example/cloneapplication/YoutubeActivity.kt
@@ -0,0 +1,11 @@
+package com.example.cloneapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class YoutubeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
